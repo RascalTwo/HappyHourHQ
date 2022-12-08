a50f150b215dd5a41b44715aaa8ef8d7199adecf
bradley carrico <BCarrico89@gmail.com>
2022-12-08 13:45:47 -0800

    Testing cors change for deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('dotenv').config({ path: './config/.env' });
 app.use(
 	cors({
 		credentials: true,
-		origin: 'http://localhost:3000',
+		origin: ['http://localhost:3000', "https://happy-hour-hq.onrender.com"]
 	})
 );
 
