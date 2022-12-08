ca4689574708740f43d994255bb5e93665e8be19
bradley carrico <BCarrico89@gmail.com>
2022-12-08 13:05:28 -0800

    uninstall nodemon

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-
 import useAuth from '../auth/useAuth';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
